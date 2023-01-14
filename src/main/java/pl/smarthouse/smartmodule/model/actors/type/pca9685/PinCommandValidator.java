8a11e8eed291666bf93fb176f08cfba1c93e9d42
majkelp1982 <s.pomazanka@gmail.com>
2023-01-14 21:49:14 +0100

    add command types

--- src/main/java/pl/smarthouse/smartmodule/model/actors/type/pca9685/PinCommandValidator.java
+++ src/main/java/pl/smarthouse/smartmodule/model/actors/type/pca9685/PinCommandValidator.java
@@ -7,9 +7,58 @@ import java.util.List;
 
 @ToString(callSuper = true)
 public class PinCommandValidator extends CommandValidator {
+  private static final int MIN_SERVO_MICROSECONDS = 0;
+  private static final int MAX_SERVO_MICROSECONDS = 4000;
 
   @Override
   protected void prepareValidationMap() {
-    commandValidationMap.put(Pca9685CommandType.WRITE_MICROSECONDS, List.of(0, 4000));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO0_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO1_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO2_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO3_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO4_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO5_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO6_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO7_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO8_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO9_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO10_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO11_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO12_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO13_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO14_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
+    commandValidationMap.put(
+        Pca9685CommandType.WRITE_SERVO15_MICROSECONDS,
+        List.of(MIN_SERVO_MICROSECONDS, MAX_SERVO_MICROSECONDS));
   }
 }
