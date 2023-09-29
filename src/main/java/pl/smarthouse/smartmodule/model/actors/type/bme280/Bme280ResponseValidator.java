7c4c95490c246ad0ad7bcc0b039f8883dccdcef1
majkelp1982 <s.pomazanka@gmail.com>
2023-09-29 20:07:13 +0200

    refactor validator

--- src/main/java/pl/smarthouse/smartmodule/model/actors/type/bme280/Bme280ResponseValidator.java
+++ src/main/java/pl/smarthouse/smartmodule/model/actors/type/bme280/Bme280ResponseValidator.java
@@ -7,22 +7,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Bme280ResponseValidator {
   public boolean isResponseValid(final String name, final Bme280Response response) {
-    boolean valid = true;
-
     if (response.getTemperature() < -10 || response.getTemperature() > 100) {
       log.warn("BME280: {}, Temperature invalid: {}", name, response.getTemperature());
-      valid = false;
+      return false;
     }
 
     if (response.getHumidity() <= 0 || response.getHumidity() > 100) {
       log.warn("BME280: {}, Humidity invalid: {}", name, response.getHumidity());
-      valid = false;
+      return false;
     }
     if (response.getPressure() < 900 || response.getPressure() > 1100) {
       log.warn("BME280: {}, Pressure invalid: {}", name, response.getPressure());
-      valid = false;
+      return false;
     }
 
-    return valid;
+    return true;
   }
 }
