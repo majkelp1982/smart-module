e70f7423777f84d405ec18ee5963496dfa95f83c
majkelp1982 <s.pomazanka@gmail.com>
2024-12-09 22:49:43 +0100

    add Bme280 I2C version

--- src/main/java/pl/smarthouse/smartmodule/model/actors/type/bme280/Bme280.java
+++ src/main/java/pl/smarthouse/smartmodule/model/actors/type/bme280/Bme280.java
@@ -19,10 +19,20 @@ public class Bme280 extends Actor {
   @JsonIgnore private Bme280CommandSet commandSet;
   @JsonIgnore private Bme280Response response = new Bme280Response();
   private int csPin;
+  int i2cAddress;
+  private Bme280Mode bme280Mode;
 
   public Bme280(@NonNull final String name, final int csPin) {
     super(ActorType.BME280, name);
     this.csPin = csPin;
+    this.bme280Mode = Bme280Mode.SPI;
+    setCommandSet(new Bme280CommandSet(Bme280CommandType.NO_ACTION));
+  }
+
+  public Bme280(@NonNull final String name, Bme280Mode mode, final int i2cAddress) {
+    super(ActorType.BME280, name);
+    this.i2cAddress = i2cAddress;
+    this.bme280Mode = mode;
     setCommandSet(new Bme280CommandSet(Bme280CommandType.NO_ACTION));
   }
 
---
+++ src/main/java/pl/smarthouse/smartmodule/model/actors/type/bme280/Bme280Mode.java
@@ -0,0 +1,6 @@
+package pl.smarthouse.smartmodule.model.actors.type.bme280;
+
+public enum Bme280Mode {
+  SPI,
+  I2C
+}
